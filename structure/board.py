b252da1761370dbf790da60c84d53125cf779d6e
toni <antonio.sanmateu@gmail.com>
2021-06-24 23:56:02 +0100

    Add board functionalities

--- structure/board.py
+++ structure/board.py
@@ -1,6 +1,22 @@
 class ChessBoard(object):
     def __init__(self):
-        self.pieces = {}
+        self.pieces = {}    # Piece: Location
 
     def reset_board(self):
         pass
+
+    def add_piece(self, piece, location):
+        # TODO: validation for extra piece or out of loc...
+        pass
+
+    @staticmethod
+    def move_piece(piece):
+        if not piece.isalive():
+            raise ValueError("This piece is dead")
+        # TODO: move drunk move...
+
+    def get_location(self, piece):
+        if piece not in self.pieces:
+            raise ValueError("Piece not in board")
+        return self.pieces[piece]
+
